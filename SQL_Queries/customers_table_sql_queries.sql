8a911113d43fb32af79b57d5e21de435884767c7
aravind-kumar-24 <aravindmpkas@gmail.com>
2025-08-24 13:06:27 +0530

    Initial data population in the database tables

--- SQL_Queries/customers_table_sql_queries.sql
+++ SQL_Queries/customers_table_sql_queries.sql
@@ -15,4 +15,18 @@ CREATE TABLE customers(
         updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         deleted_at DATETIME
 );
+
+/* SQL Insert Query */
+INSERT INTO customers (customer_name, customer_number, customer_email, customer_address)
+VALUES
+	('Aravind Kumar', '+91 9002304422', 'aravindkumar@gmail.com', 'Chennai, Tamil Nadu'),
+    ('Sri Vatsa', '+91 7896925332', 'srivatsa@gamil.com', 'Chennai, Tamil Nadu'),
+    ('Bala Ganesh', '+91 85296964213', 'balaganesh@gmail.com', 'Chennai, Tamil Nadu'),
+    ('Sharan Kumar', '+91 7536364213', 'sharan@gmail.com', 'Madurai, Tamil Nadu'),
+    ('Praveen Kumar', '+91 6353298897', 'praveen@gmail.com', 'Trichy, Tamil Nadu'),
+    ('Thanigai Raja', '+91 8529887412', 'thanigairaja@gmail.com', 'Chennai, Tamil Nadu'),
+    ('Ponraj', '+91 9845256321', 'ponraj@gmail.com', 'Karaikudi, Tamil Nadu'),
+    ('Bala Kannan', '+91 6547878931', 'balakannan@gmail.com', 'Karaikudi, Tamil Nadu'),
+    ('Madhavan', '+91 7896922352', 'madhavan@gmail.com', 'Vellore, Tamil Nadu'),
+    ('Yuvraj', '+91 6554298878', 'yuvraj@gmail.com', 'Villupuram, Tamil Nadu');
         
--- SQL_Queries/restaurants_table_sql_queries.sql
+++ SQL_Queries/restaurants_table_sql_queries.sql
@@ -15,5 +15,29 @@ CREATE TABLE restaurants(
         created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         deleted_at DATETIME
-)
+);
+
+/* ALTER Table query to drop the order_count column */
+ALTER TABLE restaurants
+DROP COLUMN order_count;
+
+/* SQL Insert Query */ 
+INSERT INTO restaurants (restaurant_name, restaurant_number, restaurant_address, restaurant_rating)
+VALUES
+	('Zaitoon', '+91 7092322223', 'Old No 57, New 15, Valluvar Kottam High Rd, Ponnangipuram, Ambal Nagar, Nungambakkam, Chennai, Tamil Nadu 600034', 4.6),
+    ("Pandia's Avenue", '+91 9884804920', 'bridge, 5, Cemetery Rd, near Royapuram, Minakshi Ammanpet, Royapuram, Chennai, Tamil Nadu 600013', 4.4),
+    ('Broken Bridge Cafe Indian Restaurant', '+91 7550017900', 'Somerset Greenways, 94, Sathyadev Ave, MRC Nagar, Raja Annamalai Puram, Chennai, Tamil Nadu 600028', 4.0),
+    ('Ratna Cafe', '+91 4428445875', 'Triplicane, Chennai, Tamil Nadu', 4.3),
+    ('Murugan Idli Shop', '+91 4428142400', 'Besant Nagar, Chennai, Tamil Nadu', 4.7),
+    ('Adyar Ananda Bhavan (A2B)', '+91 4428524350 ', 'Purasawalkam, Chennai, Tamil Nadu', 4.2),
+    ('Anjappar Chettinad Restaurant', '+91 4428276868', 'Nungambakkam High Road, Chennai, Tamil Nadu', 3.8),
+    ('Dindigul Thalappakatti', '+91 4442121111', 'Anna Nagar, Chennai, Tamil Nadu', 3.6),
+    ('The Patiala House', '+91 4448619292', 'Chetpet, Chennai, Tamil Nadu', 3.9),
+    ('ZA Pizza Cafe', '+91 4443095050', 'Nungambakkam, Chennai, Tamil Nadu', 3.4),
+    ('Beat Route', '+91 4443562222', 'Nungambakkam, Chennai, Tamil Nadu', 3.5),
+    ('Moonlight Take Away & Delivery', '+91 4428154545', 'T Nagar, Chennai, Tamil Nadu', 3.9),
+    ('Sangeetha Veg Restaurant', '+91 4428151234', 'Mylapore, Chennai, Tamil Nadu', 4.0),
+    ('Buhari Hotel', '+91 4428552550', 'Mount Road, Chennai, Tamil Nadu', 4.8);
+    
+    
         
