a669b3e9362c96d6948108063fab73a94a458197
aravind-kumar-24 <aravindmpkas@gmail.com>
2025-08-24 18:12:59 +0530

    Insert some records in the orders table

--- SQL_Queries/orders_table_sql_queries.sql
+++ SQL_Queries/orders_table_sql_queries.sql
@@ -25,3 +25,17 @@ CREATE TABLE orders(
 /* SQL ALTER TABLE Query */
 ALTER TABLE orders
 ADD FOREIGN KEY (customer_id) REFERENCES customers(customer_id); 
+
+ALTER TABLE orders
+ADD COLUMN order_number INT NOT NULL AFTER order_id;
+
+/* SQL Insert Query (Order against single user for time being)*/
+INSERT INTO orders (order_number, restaurant_id, customer_id, item_id, item_price, qty, total_amount, status)
+VALUES
+	(1, 1, 1, 9, '350', '1', CAST(350 * 1 AS CHAR), 0),
+    (1, 1, 1, 11, '340', '1', CAST(340 * 1 AS CHAR), 0),
+    (1, 1, 1, 28, '475', '2', CAST(475 * 2 AS CHAR), 0),
+    (1, 1, 1, 25, '250', '2', CAST(250 * 2 AS CHAR), 0),
+	(2, 4, 3, 81, '255', '1', CAST(255 * 1 AS CHAR), 0),
+    (2, 4, 3, 89, '105', '2', CAST(105 * 2 AS CHAR), 0),
+    (2, 4, 3, 92, '160', '2', CAST(160 * 2 AS CHAR), 0);
